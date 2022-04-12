261f96068b4e0993314b0dee884829f9cf50d880
EsmondKim <ekelectronicmail@gmail.com>
2022-04-11 21:44:44 -0500

    Unique email validation check on registration page.

---
+++ src/main/java/teksystems/esmondkimcasestudy/validation/EmailUniqueImpl.java
@@ -0,0 +1,35 @@
+package teksystems.esmondkimcasestudy.validation;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import teksystems.esmondkimcasestudy.database.dao.UserDAO;
+import teksystems.esmondkimcasestudy.database.entity.User;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class EmailUniqueImpl implements ConstraintValidator<EmailUnique, String> {
+
+    public static final Logger LOG = LoggerFactory.getLogger(EmailUniqueImpl.class);
+
+    @Autowired
+    private UserDAO UserDAO;
+
+    @Override
+    public void initialize(EmailUnique constraintAnnotation) {
+
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if ( StringUtils.isEmpty(value) ) {
+            return true;
+        }
+
+        User user = UserDAO.findByEmail(value);
+        return(user == null);
+    }
+
+}
